fac6c31f618023add4b386b94b00e7e5c40b01df
Jace Browning <jacebrowning@gmail.com>
2014-12-05 22:29:05 -0500

    Remove integration test with broken URL

--- curmit/test/test_curmit.py
+++ curmit/test/test_curmit.py
@@ -13,9 +13,6 @@ from curmit.curmit import main, urltext
 ENV = 'TEST_INTEGRATION'  # environment variable to enable integration tests
 REASON = "'{0}' variable not set".format(ENV)
 
-# This published document cannot be parsed as a Google doc by 'html2text'
-STRANGE_URL = "https://docs.google.com/document/d/1DXQk7Qnd0wCBZ4T8GcUHcMMc2oVPMjXOxMIeyfLQ4v4/pub?embedded=true"  # pylint: disable=C0301
-
 
 class TestCLI(unittest.TestCase):  # pylint: disable=R0904
     """Integration tests for the 'curmit' command."""
@@ -61,10 +58,6 @@ class TestLogging(unittest.TestCase):  # pylint: disable=R0904
 @unittest.skipUnless(os.getenv(ENV), REASON)  # pylint: disable=R0904
 class TestUrlText(unittest.TestCase):  # pylint: disable=R0904
     """Integration tests for getting URL text."""
-
-    def test_invalid_google_doc(self):
-        """Verify that 'html2text' handles strange Google Documents."""
-        self.assertIsInstance(urltext(STRANGE_URL), list)
 
     def test_invalid(self):
         """Verify an exception is raised on an invalid URL."""
