296e31f9b9f8d313e647e88d1112bd58c9f31182
Jace Browning <jacebrowning@gmail.com>
2014-12-06 18:17:51 -0500

    Add an integration test for grabbing text

--- curmit/test/test_curmit.py
+++ curmit/test/test_curmit.py
@@ -5,11 +5,14 @@
 import os
 import unittest
 from unittest.mock import patch, Mock
+import logging
 
-from curmit.curmit import main
+from curmit.curmit import main, urltext
 
 ENV = 'TEST_INTEGRATION'  # environment variable to enable integration tests
 REASON = "'{0}' variable not set".format(ENV)
+
+SAMPLE_URL = "https://docs.google.com/document/d/1UamfLkA-DvIVXPKoFQpSQDIUDANPTfyyXYMlUHmKpp4/pub?embedded=True"
 
 
 class TestCLI(unittest.TestCase):  # pylint: disable=R0904
@@ -51,3 +54,15 @@ class TestLogging(unittest.TestCase):  # pylint: disable=R0904
     def test_verbose_3(self):
         """Verify verbose level 3 can be set."""
         self.assertIs(None, main(['-vvv']))
+
+
+@unittest.skipUnless(os.getenv(ENV), REASON)  # pylint: disable=R0903,R0904
+class TestUrlText(unittest.TestCase):
+    """Integration tests for getting URL text."""
+
+    def test_sample(self):
+        """Verify text is grabbed from a URL."""
+        lines = urltext(SAMPLE_URL)
+        for line in lines:
+            logging.debug(line)
+        self.assertIn("Sample File", lines[0])
